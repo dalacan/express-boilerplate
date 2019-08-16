fa3a888dc755d69e1f86860dff27e230d4d5e238
dalacan <dalacan@gmail.com>
2019-08-16 23:48:27 +1000

    Updated last commit sha to use environment variable.

--- routes/status.js
+++ routes/status.js
@@ -1,16 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-lastcommitsha = require('child_process')
-  .execSync('git rev-parse HEAD')
-  .toString().trim()
-
 var status = {
   [process.env.npm_package_name] : [
     {
       "version": process.env.npm_package_version,
       "description": process.env.npm_package_description,
-      "lastcommitsha": lastcommitsha
+      "lastcommitsha": process.env.LAST_COMMIT_SHA
     },
   ]
 }
