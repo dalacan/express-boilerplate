709bab910404cb09e62805c6d0c2af86139cbda3
dalacan <dalacan@gmail.com>
2019-08-16 22:07:53 +1000

    Refactored code for a separate index.

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 var http = require('http');
 var express = require('express');
 
+var indexRouter = require('./routes/index');
 var statusRouter = require('./routes/status');
 
 var app = express();
 
 app.use(express.json());
 
+app.use('/', indexRouter);
 app.use('/status', statusRouter);
 
-app.use('/', function(req, res) {
-    res.send('hello world');
-});
+// app.use('/', function(req, res) {
+//     res.send('hello worlds');
+// });
 
 var server = http.createServer(app);
 var port = 3000;
