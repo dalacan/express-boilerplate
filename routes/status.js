48851951b45eb8e46da92c153000f420fe90bbde
dalacan <dalacan@gmail.com>
2019-08-16 23:56:47 +1000

    Updated to either use environment or local sha.

--- routes/status.js
+++ routes/status.js
@@ -1,12 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+if(process.env.LAST_COMMIT_SHA) {
+  last_commit_sha = process.env.LAST_COMMIT_SHA;
+}
+else {
+  last_commit_sha = require('child_process')
+      .execSync('git rev-parse HEAD')
+      .toString().trim()  
+}
+
 var status = {
   [process.env.npm_package_name] : [
     {
       "version": process.env.npm_package_version,
       "description": process.env.npm_package_description,
-      "lastcommitsha": process.env.LAST_COMMIT_SHA
+      "lastcommitsha": last_commit_sha
     },
   ]
 }
